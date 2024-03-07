7ce035d4c75baea2b871479c0c1934a8b1670ffe
wknishio <wknishio@gmail.com>
2024-03-07 12:38:24 -0300

    updated packet communications

--- src/vate/org/vash/vate/client/connection/VTClientConnection.java
+++ src/vate/org/vash/vate/client/connection/VTClientConnection.java
@@ -1165,15 +1165,15 @@ public class VTClientConnection
   
   public void resetGraphicsModeStreams() throws IOException
   {
-    graphicsControlInputStream.open();
     graphicsControlOutputStream.open();
+    //graphicsControlInputStream.open();
     
     graphicsDirectImageOutputStream.open();
-    graphicsDirectImageInputStream.open();
+    //graphicsDirectImageInputStream.open();
     graphicsHeavyImageOutputStream.open();
-    graphicsHeavyImageInputStream.open();
+    //graphicsHeavyImageInputStream.open();
     graphicsFastImageOutputStream.open();
-    graphicsFastImageInputStream.open();
+    //graphicsFastImageInputStream.open();
     
     graphicsControlDataInputStream.setIntputStream(VTCompressorSelector.createBufferedLz4InputStream(graphicsControlInputStream));
     graphicsControlDataOutputStream.setOutputStream(VTCompressorSelector.createBufferedLz4OutputStream(graphicsControlOutputStream));
@@ -1212,7 +1212,7 @@ public class VTClientConnection
     // graphicsControlInputStream.removePropagated(clipboardDataInputStream);
     
     graphicsClipboardOutputStream.open();
-    graphicsClipboardInputStream.open();
+    //graphicsClipboardInputStream.open();
     
     clipboardDataOutputStream = VTCompressorSelector.createBufferedZstdOutputStream(graphicsClipboardOutputStream);
     clipboardDataInputStream = VTCompressorSelector.createBufferedZstdInputStream(graphicsClipboardInputStream);
@@ -1224,7 +1224,7 @@ public class VTClientConnection
   public void resetFileTransferStreams() throws IOException
   {
     fileTransferDataOutputStream.open();
-    fileTransferDataInputStream.open();
+    //fileTransferDataInputStream.open();
   }
   
   public void closeFileTransferStreams() throws IOException
@@ -1242,7 +1242,7 @@ public class VTClientConnection
   public void resetAudioStreams() throws IOException
   {
     audioDataOutputStream.open();
-    audioDataInputStream.open();
+    //audioDataInputStream.open();
     // audioDataOutputStream = audioOutputStream;
     // audioDataInputStream = audioInputStream;
   }
