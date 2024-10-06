55f9e90f47e2ecb98393dd059d0a7c52de6a45c2
wknishio <wknishio@gmail.com>
2024-10-06 09:04:46 -0300

    updated console api

--- src/vate/org/vash/vate/console/standard/VTStandardConsoleInterruptibleInputStream.java
+++ src/vate/org/vash/vate/console/standard/VTStandardConsoleInterruptibleInputStream.java
@@ -28,6 +28,11 @@ public class VTStandardConsoleInterruptibleInputStream extends InputStream
     reader.setEcho(echo);
   }
   
+  public boolean getEcho()
+  {
+    return reader.getEcho();
+  }
+  
   public boolean usingRead()
   {
     return currentThread == null;
@@ -112,5 +117,10 @@ public class VTStandardConsoleInterruptibleInputStream extends InputStream
   public void close() throws IOException
   {
     // unclosable!
+  }
+  
+  public void input(String data)
+  {
+    reader.input(data);
   }
 }
