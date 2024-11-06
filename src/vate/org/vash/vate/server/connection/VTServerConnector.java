0e4b42f66e1f9b55dde49f523ae432bc0ed25c2c
wknishio <wknishio@gmail.com>
2024-11-06 16:30:38 -0300

    added ping interval and ping limit parameters

--- src/vate/org/vash/vate/server/connection/VTServerConnector.java
+++ src/vate/org/vash/vate/server/connection/VTServerConnector.java
@@ -452,7 +452,7 @@ public class VTServerConnector implements Runnable
       connection.getConnectionSocket().setTcpNoDelay(true);
       //connection.getConnectionSocket().setSoLinger(true, 0);
       //connection.getConnectionSocket().setKeepAlive(true);
-      connection.getConnectionSocket().setSoTimeout(server.getDataTimeout());
+      connection.getConnectionSocket().setSoTimeout(server.getPingLimitMilliseconds());
 //      connecting = false;
       if (encryptionType == null)
       {
@@ -516,7 +516,7 @@ public class VTServerConnector implements Runnable
       connection.getConnectionSocket().setTcpNoDelay(true);
       //connection.getConnectionSocket().setSoLinger(true, 0);
       //connection.getConnectionSocket().setKeepAlive(true);
-      connection.getConnectionSocket().setSoTimeout(server.getDataTimeout());
+      connection.getConnectionSocket().setSoTimeout(server.getPingLimitMilliseconds());
 //      connecting = false;
       if (encryptionType == null)
       {
